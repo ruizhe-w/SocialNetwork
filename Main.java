97ae3f3f754a81240a23df42f52282ceed446a73
98729 <53661912+98729@users.noreply.github.com>
2019-12-03 17:34:31 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 /**
- * 
+ * CS400 final project 
+ * Team: ateam 166
+ * Member: 1. Yijun Cheng, lecture 001, cheng229@wisc.edu
+ *		   2. Yuedong Cui, lecture 001, cui54@wisc.edu
+ *		   3. Ruizhe Wang, lecture 001, rwang477@wisc.edu
+ *		   4. Yuzheng Zhang, lecture 001, yzhang975@wisc.edu
+ *		   5. Haolin Li, lecture 001, hli564@wisc.edu
+ * This class is used to create a scene to show a social network
  */
 package application;
 
@@ -12,32 +19,34 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-/**
- * @author 95646
- *
- */
-public class Main extends Application{
+public class Main extends Application {
 	private List<String> args;
 
+	// set the layout
 	private static final int WINDOW_WIDTH = 1050;
 	private static final int WINDOW_HEIGHT = 600;
 	private static final String APP_TITLE = "Social Network Visualizer";
 
+	/**
+	 * This method is used to create a social network pane
+	 */
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		args = this.getParameters().getRaw();
+		// set the title
 		primaryStage.setTitle(APP_TITLE);
+		// create the menu pane and visual pane in a vertical box
 		VBox vBox = new VBox(new MenuPane(), new VisualPane());
+		// create root pane
 		BorderPane root = new BorderPane();
 		root.setCenter(vBox);
-		
+		// create scene
 		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
-		
+
 		primaryStage.setScene(mainScene);
 		primaryStage.show();
 	}
-	
-	
+
 	/**
 	 * @param args
 	 */
