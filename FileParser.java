ba25086ac4bffa11499a8c62d796c46ee68e29e5
Ruizhe <ruizhe@cs.wisc.edu>
2019-12-07 18:32:03 -0600

    bug fixed

--- FileParser.java
+++ FileParser.java
@@ -1,32 +1,69 @@
 /**
- * 
+ * CS400 final project 
+ * Team: ateam 166
+ * Member: 1. Yijun Cheng, lecture 001, cheng229@wisc.edu
+ *		   2. Yuedong Cui, lecture 001, cui54@wisc.edu
+ *		   3. Ruizhe Wang, lecture 001, rwang477@wisc.edu
+ *		   4. Yuzheng Zhang, lecture 001, yzhang975@wisc.edu
+ *		   5. Haolin Li, lecture 001, hli564@wisc.edu
+ * This class is used to read instructions from file and save instructions 
+ * to file
  */
 package application;
 
-/**
- * Finish all operations related to files
- * 
- * @author Ruizhe Wang
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FileParser {
 
 	
 	/**
 	 * load a file from the given filename
 	 * @param fileName
-	 * @return true if load correctly
+	 * @throws IOException 
 	 */
-	public static boolean loadFromFile(VisualPane visualPane, String fileName) {
-		return false;
+	public static void loadFromFile(VisualPane visualPane, String fileName) throws IOException {
+		File file = new File(fileName);
+		Scanner scnr = new Scanner(file);
+		while (scnr.hasNextLine()) {
+			String[] str = scnr.nextLine().split(" ");
+			if (str[0].equals("a")) {
+				if (str.length == 2) {
+					visualPane.addVertex(str[1]);
+				} else if (str.length == 3){
+					visualPane.addEdge(str[1], str[2]);
+				}
+			} else if (str[0].equals("r")) {
+				if (str.length == 2) {
+					visualPane.removeVertex(str[1]);
+				} else if (str.length == 3){
+					visualPane.removeEdge(str[1], str[2]);
+				}
+			} else if (str[0].equals("s")) {
+				// TODO: clean first
+				ArrayList<String> friends = visualPane.setCentralUser(str[1]);
+				visualPane.addVertex(str[1]);
+				for (int i = 0; i < friends.size(); i++) {
+					visualPane.addEdge(str[1], friends.get(i));
+				}
+			}
+		}
+		scnr.close();
 	}
 	
 	
 	/**
 	 * save current layout to a new file
 	 * @param visualPane
-	 * @return true if save correctly
+	 * @throws IOException 
 	 */
-	public static boolean saveToFile(VisualPane visualPane) {
-		return false;
+	public static void saveToFile(VisualPane visualPane, String filename) throws IOException {
+		File file = new File(filename);
+		PrintWriter printer = new PrintWriter(file);
+		// TODO: click function
 	}
 }
---
+++ README.md
@@ -0,0 +1,35 @@
+README
+
+Course: cs400
+Semester: Fall 2019
+Project name: Social Network
+Team Members:
+1. Yijun Cheng, lecture 001, cheng229@wisc.edu
+2. Yuedong Cui, lecture 001, cui54@wisc.edu
+3. Ruizhe Wang, lecture 001, rwang477@wisc.edu
+4. Yuzheng Zhang, lecture 001, yzhang975@wisc.edu
+5. Haolin Li, lecture 001, hli564@wisc.edu
+ 
+
+Which team members were on same xteam together?
+NONE
+
+
+Other notes or comments to the grader:
+This is a a program that displays a GUI that shows the main view that the user
+will see for your program.  This is a initial view of this program, which shows
+all of the users in the screen and their relationship. Later, you can click each
+user to set him/her to the central user. But now, in the initial view, there is
+no central user.
+
+[place any comments or notes that will help the grader here]
+In this program, we have a search bar used to search user name. "add" function 
+used to add any new user or edge into the network. "remove" function used to 
+remove any user or edge in the network. "Mutual Friends" used to view a list of 
+mutual friends between any two users. "shortest path" used to find and display
+the sequence of friends that is the shortest path between any two users within 
+a connected component. "Number of groups" used to View an integer that indicates
+the number of connected components (groups) in the social network. 
+"Last instruction" used to display a status field that indicates what was just
+done. "clean" button used to clean current network. "exit" button used to exit
+the program. "save" button allowed user to save the instructions into a file.
